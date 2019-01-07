00ae6c3a6453b29f2a2e7f28da663dfdfcff7557
ydh0520 <ydh2338@naver.com>
2019-01-07 20:45:23 +0900

    회원 가입 & 비밀번호 찾기 테플릿 추가

--- components/Login.vue
+++ components/Login.vue
@@ -1,15 +1,36 @@
 <template>
     <div>
-        <input type="text">
-        <input type="password">
-        <button>로그인하기</button>
-        <button>회원가입하기</button>
+        <div v-if="type==0">
+            <input type="text">
+            <input type="password">
+            <button>로그인하기</button>
+            <button @click="type=1">회원가입하기</button>
+            <button @click="type=2">비밀번호 찾기</button>
+        </div>
+        <div v-if="type==1">
+            <input type="text">
+            <input type="password">
+            <input type="text">
+            <input type="text">
+            <input type="text">
+            <button>가입하기</button>
+        </div>
+        <div v-if="type==2">
+            <input type="text">
+            <input type="email">
+            <button>비밀번호 찾기</button>
+            <button @click="type=0">로그인하기</button>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
-
+    data(){
+        return{
+            type:0
+        }
+    }
 }
 </script>
 
