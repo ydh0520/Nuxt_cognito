67d802f7ce987d2a0bfab3935d372461b71c8a72
ydh0520 <ydh2338@naver.com>
2019-01-07 11:44:49 +0900

    main page

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
-    hello world
+    <login></login>
+    <mypage></mypage>
   </div>
 </template>
 
 <script>
+import Login from "~/components/Login"
+import Mypage from "~/components/Mypage"
 export default {
-
+  components:{
+    Login,
+    Mypage
+  }
 }
 </script>
 
