762a5bcccb74aabcc6001aa0070c1e64df558db7
ydh0520 <ydh2338@naver.com>
2019-01-07 11:44:35 +0900

    login

---
+++ components/Login.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <input type="text">
+        <input type="password">
+        <button>로그인하기</button>
+        <button>회원가입하기</button>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
