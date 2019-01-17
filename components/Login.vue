fd60e79155ef31f41260ffe277eba4693913a38c
ydh0520 <ydh2338@naver.com>
2019-01-17 17:36:27 +0900

    add event

--- components/Login.vue
+++ components/Login.vue
@@ -1,20 +1,23 @@
 <template>
     <div>
+        <!-- 로그인 창 -->
         <div v-if="type==0">
-            <input type="text">
-            <input type="password">
+            <input type="email" v-model="id">
+            <input type="password" v-model="password">
             <button @click="login()">로그인하기</button>
             <button @click="type=1">회원가입하기</button>
             <button @click="type=2">비밀번호 찾기</button>
         </div>
+        <!-- 회원가입 창 -->
         <div v-if="type==1">
+            <input type="text" v-model="id">
+            <input type="password" v-model="password">
             <input type="text">
-            <input type="password">
             <input type="text">
             <input type="text">
-            <input type="text">
-            <button>가입하기</button>
+            <button @click="signUp()">가입하기</button>
         </div>
+        <!-- 비밀번호 찾기 창 -->
         <div v-if="type==2">
             <input type="text">
             <input type="email">
@@ -25,15 +28,33 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
     data(){
         return{
-            type:0
+            type:0,
+            id:'',
+            password:'',
+            verifyCode:''
         }
     },
     methods:{
         login(){
-            this.$store.commit('login')
+            axios.post('/login',{
+                id:this.id,
+                password:this.password
+            }).then((result)=>{
+
+            })
+        },
+        signUp(){
+            axios.post('/signUp',{
+                id:this.id,
+                password:this.password
+            }).then((result)=>{
+
+            })
         }
     }
 }
